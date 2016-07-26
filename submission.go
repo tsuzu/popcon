becbd73153a86ec4f2a16db0bedad876e2be9958
tsuzu <tsuzu.0623@gmail.com>
2016-07-26 17:51:55 +0900

    Add SubmissionCount() in submission.go and Gofmt

--- submission.go
+++ submission.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"time"
-	"os"
-	"strconv"
+	"encoding/json"
 	"errors"
 	"io/ioutil"
+	"os"
+	"strconv"
+	"time"
+	"strings"
 	"github.com/cs3238-tsuzu/popcon/file_manager"
-	"encoding/json"
 )
 
 // Database manager for Contest and Onlinejudge
@@ -19,275 +20,346 @@ const SubmissionDir = "./submissions/"
 type SubmissionStatus int64
 
 const (
-    InQueue SubmissionStatus = 0
-    Judging SubmissionStatus = 1
-    Accepted SubmissionStatus = 2
-    WrongAnswer SubmissionStatus = 3
-    TimeLimitExceeded SubmissionStatus = 4
-    MemoryLimitExceeded SubmissionStatus = 5
-    RuntimeError SubmissionStatus = 7
-    CompileError SubmissionStatus = 8
-    InternalError SubmissionStatus = 9
+	InQueue             SubmissionStatus = 0
+	Judging             SubmissionStatus = 1
+	Accepted            SubmissionStatus = 2
+	WrongAnswer         SubmissionStatus = 3
+	TimeLimitExceeded   SubmissionStatus = 4
+	MemoryLimitExceeded SubmissionStatus = 5
+	RuntimeError        SubmissionStatus = 7
+	CompileError        SubmissionStatus = 8
+	InternalError       SubmissionStatus = 9
 )
 
 var SubmissionStatusToString = map[SubmissionStatus]string{
-    InQueue: "WJ",
-    Judging: "JG",
-    Accepted: "AC",
-    WrongAnswer: "WA",
-    TimeLimitExceeded: "TLE",
-    MemoryLimitExceeded: "MLE",
-    RuntimeError: "RE",
-    CompileError: "CE",
-    InternalError: "IE",
+	InQueue:             "WJ",
+	Judging:             "JG",
+	Accepted:            "AC",
+	WrongAnswer:         "WA",
+	TimeLimitExceeded:   "TLE",
+	MemoryLimitExceeded: "MLE",
+	RuntimeError:        "RE",
+	CompileError:        "CE",
+	InternalError:       "IE",
 }
 
 type Submission struct {
-    Sid int64 `db:"pk" default:""`
-    Pid int64 `default:""` //index
-    Cid int64 `default:""` //index, onlinejudge->0
-    Iid int64 `default:""` //index
-    Lang int64 `default:""`
-    Time int64 `default:""` //ms
-    Mem int64 `default:""` //KB
-    Score int64 `default:""`
-    SubmitTime int64 `default:""` //提出日時
-    Status int64 `default:""` //index
-    Prog uint64 `default:""` //テストケースの進捗状況(完了数<<32 & 全体数)
+	Sid        int64  `db:"pk" default:""`
+	Pid        int64  `default:""` //index
+	Cid        int64  `default:""` //index, onlinejudge->0
+	Iid        int64  `default:""` //index
+	Lang       int64  `default:""`
+	Time       int64  `default:""` //ms
+	Mem        int64  `default:""` //KB
+	Score      int64  `default:""`
+	SubmitTime int64  `default:""` //提出日時
+	Status     int64  `default:""` //index
+	Prog       uint64 `default:""` //テストケースの進捗状況(完了数<<32 & 全体数)
 }
 
 func (dm *DatabaseManager) CreateSubmissionTable() error {
-    err := dm.db.CreateTableIfNotExists(&Submission{})
+	err := dm.db.CreateTableIfNotExists(&Submission{})
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    dm.db.CreateIndex(&Submission{}, "pid")
-    dm.db.CreateIndex(&Submission{}, "cid")
-    dm.db.CreateIndex(&Submission{}, "iid")
-    dm.db.CreateIndex(&Submission{}, "status")
+	dm.db.CreateIndex(&Submission{}, "pid")
+	dm.db.CreateIndex(&Submission{}, "cid")
+	dm.db.CreateIndex(&Submission{}, "iid")
+	dm.db.CreateIndex(&Submission{}, "status")
 
-    return nil
+	return nil
 }
 
 func (dm *DatabaseManager) SubmissionNew(pid, cid, iid, lang int64, code string) (int64, error) {
-    sm := Submission {
-        Pid: pid,
-        Cid: cid,
-        Iid: iid,
-        Lang: lang,
-        Time: 0,
-        Mem: 0,
-        Score: 0,
-        SubmitTime: time.Now().Unix(),
-        Status: int64(InQueue),
-        Prog: 0,
-    }
+	sm := Submission{
+		Pid:        pid,
+		Cid:        cid,
+		Iid:        iid,
+		Lang:       lang,
+		Time:       0,
+		Mem:        0,
+		Score:      0,
+		SubmitTime: time.Now().Unix(),
+		Status:     int64(InQueue),
+		Prog:       0,
+	}
 
-    _, err := dm.db.Insert(&sm)
-    id := sm.Sid
+	_, err := dm.db.Insert(&sm)
+	id := sm.Sid
 
-    if err != nil {
-        return 0, err
-    }
+	if err != nil {
+		return 0, err
+	}
 
-    err = os.MkdirAll(SubmissionDir + strconv.FormatInt(id, 10), os.ModePerm)
+	err = os.MkdirAll(SubmissionDir+strconv.FormatInt(id, 10), os.ModePerm)
 
-    if err != nil {
-        dm.SubmissionRemove(id)
+	if err != nil {
+		dm.SubmissionRemove(id)
 
-        return 0, err
-    }
+		return 0, err
+	}
 
-    fp, err := os.OpenFile(SubmissionDir + strconv.FormatInt(id, 10) + "/msg", os.O_WRONLY | os.O_CREATE, 0644)
+	fp, err := os.OpenFile(SubmissionDir+strconv.FormatInt(id, 10)+"/msg", os.O_WRONLY|os.O_CREATE, 0644)
 
-    if err != nil {
-        dm.SubmissionRemove(id)
+	if err != nil {
+		dm.SubmissionRemove(id)
 
-        return 0, err
-    }
+		return 0, err
+	}
 
-    fp.Close()
+	fp.Close()
 
-    fp, err = os.OpenFile(SubmissionDir + strconv.FormatInt(id, 10) + "/case", os.O_WRONLY | os.O_CREATE, 0644)
+	fp, err = os.OpenFile(SubmissionDir+strconv.FormatInt(id, 10)+"/case", os.O_WRONLY|os.O_CREATE, 0644)
 
-    if err != nil {
-        dm.SubmissionRemove(id)
+	if err != nil {
+		dm.SubmissionRemove(id)
 
-        return 0, err
-    }
+		return 0, err
+	}
 
-    fp.Close()
+	fp.Close()
 
-    fp, err = os.OpenFile(SubmissionDir + strconv.FormatInt(id, 10) + "/code", os.O_WRONLY | os.O_CREATE, 0644)
+	fp, err = os.OpenFile(SubmissionDir+strconv.FormatInt(id, 10)+"/code", os.O_WRONLY|os.O_CREATE, 0644)
 
-    if err != nil {
-        dm.SubmissionRemove(id)
+	if err != nil {
+		dm.SubmissionRemove(id)
 
-        return 0, err
-    }
+		return 0, err
+	}
 
-    defer fp.Close()
+	defer fp.Close()
 
-    _, err = fp.Write([]byte(code))
+	_, err = fp.Write([]byte(code))
 
-    if err != nil {
-        return 0, err
-    }
+	if err != nil {
+		return 0, err
+	}
 
-    return id, nil
+	return id, nil
 }
 
 func (dm *DatabaseManager) SubmissionRemove(sid int64) error {
-    sm := Submission{Sid: sid}
-    _, err := dm.db.Delete(&sm)
-    
-    if err != nil {
-        return err
-    }
+	sm := Submission{Sid: sid}
+	_, err := dm.db.Delete(&sm)
 
-    fm1, _ := FileManager.OpenFile(SubmissionDir + strconv.FormatInt(sid, 10) + "/case", os.O_RDONLY, true)
-    fm2, _ := FileManager.OpenFile(SubmissionDir + strconv.FormatInt(sid, 10) + "/msg", os.O_RDONLY, true)
-    
-    defer func () {
-        fm1.Close()
-        fm2.Close()
-    }()
+	if err != nil {
+		return err
+	}
 
-    return os.RemoveAll(SubmissionDir + strconv.FormatInt(sid, 10))
+	fm1, _ := FileManager.OpenFile(SubmissionDir+strconv.FormatInt(sid, 10)+"/case", os.O_RDONLY, true)
+	fm2, _ := FileManager.OpenFile(SubmissionDir+strconv.FormatInt(sid, 10)+"/msg", os.O_RDONLY, true)
+
+	defer func() {
+		fm1.Close()
+		fm2.Close()
+	}()
+
+	return os.RemoveAll(SubmissionDir + strconv.FormatInt(sid, 10))
 }
 
 func (dm *DatabaseManager) SubmissionFind(sid int64) (*Submission, error) {
-    var results []Submission
+	var results []Submission
 
-    err := dm.db.Select(&results, dm.db.Where("sid", "=", sid))
+	err := dm.db.Select(&results, dm.db.Where("sid", "=", sid))
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    if len(results) == 0 {
-        return nil, errors.New("Unknown submission")
-    }
+	if len(results) == 0 {
+		return nil, errors.New("Unknown submission")
+	}
 
-    return &results[0], nil
+	return &results[0], nil
 }
 
 func (dm *DatabaseManager) SubmissionUpdate(sid, time, mem int64, status SubmissionStatus, fin, all int) error {
-    sm, err := dm.SubmissionFind(sid)
+	sm, err := dm.SubmissionFind(sid)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    sm.Time = time
-    sm.Mem = mem
-    sm.Status = int64(status)
-    sm.Prog = uint64(fin) << 32 & uint64(all)
+	sm.Time = time
+	sm.Mem = mem
+	sm.Status = int64(status)
+	sm.Prog = uint64(fin) << 32 & uint64(all)
 
-    _, err = dm.db.Update(&sm)
+	_, err = dm.db.Update(&sm)
 
-    return err
+	return err
 }
 
 func (dm *DatabaseManager) SubmissionGetCode(sid int64) (*string, error) {
-    fp, err := os.OpenFile(SubmissionDir + strconv.FormatInt(sid, 10) + "/code", os.O_RDONLY, 0644)
+	fp, err := os.OpenFile(SubmissionDir+strconv.FormatInt(sid, 10)+"/code", os.O_RDONLY, 0644)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    defer fp.Close()
+	defer fp.Close()
 
-    b, err := ioutil.ReadAll(fp)
+	b, err := ioutil.ReadAll(fp)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    str := string(b)
+	str := string(b)
 
-    return &str, nil
+	return &str, nil
 }
 
 func (dm *DatabaseManager) SubmissionGetMsg(sid int64) string {
-    fm, err := FileManager.OpenFile(SubmissionDir + strconv.FormatInt(sid, 10) + "/msg", os.O_RDONLY, false)
-    
-    if err != nil {
-        return ""
-    }
+	fm, err := FileManager.OpenFile(SubmissionDir+strconv.FormatInt(sid, 10)+"/msg", os.O_RDONLY, false)
 
-    defer fm.Close()
+	if err != nil {
+		return ""
+	}
 
-    b, err := ioutil.ReadAll(fm)
+	defer fm.Close()
 
-    if err != nil {
-        return ""
-    }
+	b, err := ioutil.ReadAll(fm)
 
-    return string(b)
+	if err != nil {
+		return ""
+	}
+
+	return string(b)
 }
 
 func (dm *DatabaseManager) SubmissionSetMsg(sid int64, msg string) error {
-    fm, err := FileManager.OpenFile(SubmissionDir + strconv.FormatInt(sid, 10) + "/msg", os.O_WRONLY | os.O_CREATE, true)
+	fm, err := FileManager.OpenFile(SubmissionDir+strconv.FormatInt(sid, 10)+"/msg", os.O_WRONLY|os.O_CREATE, true)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    defer fm.Close()
-    
-    _, err = fm.Write([]byte(msg))
+	defer fm.Close()
 
-    return err
+	_, err = fm.Write([]byte(msg))
+
+	return err
 }
 
 func (dm *DatabaseManager) SubmissionGetCase(sid int64) (*map[int64]SubmissionStatus, error) {
-    fm, err := FileManager.OpenFile(SubmissionDir + strconv.FormatInt(sid, 10) + "/case", os.O_RDONLY, false)
-    
-    if err != nil {
-        return nil, err
-    }
-    
-    defer fm.Close()
+	fm, err := FileManager.OpenFile(SubmissionDir+strconv.FormatInt(sid, 10)+"/case", os.O_RDONLY, false)
 
-    b, err := ioutil.ReadAll(fm)
+	if err != nil {
+		return nil, err
+	}
 
-    if err != nil {
-        return nil, err
-    }
+	defer fm.Close()
 
-    var ss map[int64]SubmissionStatus
-    err = json.Unmarshal(b, &ss)
+	b, err := ioutil.ReadAll(fm)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    return &ss, nil
+	var ss map[int64]SubmissionStatus
+	err = json.Unmarshal(b, &ss)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &ss, nil
 }
 
 func (dm *DatabaseManager) SubmissionSetCase(sid int64, ss map[int64]SubmissionStatus) error {
-    fm, err := FileManager.OpenFile(SubmissionDir + strconv.FormatInt(sid, 10) + "/case", os.O_RDONLY, false)
-    
-    if err != nil {
-        return err
-    }
-    
-    defer fm.Close()
+	fm, err := FileManager.OpenFile(SubmissionDir+strconv.FormatInt(sid, 10)+"/case", os.O_RDONLY, false)
 
-    b, err := json.Marshal(ss)
+	if err != nil {
+		return err
+	}
 
-    if err != nil {
-        return err
-    }
+	defer fm.Close()
 
-    _, err = fm.Write(b)
+	b, err := json.Marshal(ss)
 
-    return err
+	if err != nil {
+		return err
+	}
+
+	_, err = fm.Write(b)
+
+	return err
 }
 
-func (dm *DatabaseManager) SubmissionCount(cid, iid, lid int64) {
-    //
+type SubmissionView struct {
+	SubmitTime int64
+	Cid int64
+	Pidx int64
+	Name int64
+	Uid string
+	UserName string
+	Lang string
+	Score int64
+	Status string
+	Time int64
+	Mem int64
+	Sid int64
+}
+
+func (dm *DatabaseManager) SubmissionCount(cid, iid, lid, pidx, stat, offset, limit int64) (*[]SubmissionView, error) {
+	placeholders := make([]interface{}, 0, 5)
+	conditions := make([]string, 0, 5)
+
+	query := "select submission.submit_time, submission.cid, contest_problem.pidx, contest_problem.name, user.uid, user.user_name, language.name, submission.score, submission.status, submission.prog, submission.time, submission.mem, submission.sid from submission inner join contest_problem on submission.pid = contest_problem.pid inner join user on submission.iid = user.iid inner join language on submission.lang = language.lid "
+
+	if cid != -1 {
+		conditions = append(conditions, "submission.cid = ? ")
+		placeholders = append(placeholders, cid)
+	}
+
+	if iid != -1 {
+		conditions = append(conditions, "user.iid = ? ")
+		placeholders = append(placeholders, iid)
+	}
+
+	if pidx != -1 {
+		conditions = append(conditions, "contest_problem.pidx = ? ")
+		placeholders = append(placeholders, pidx)
+	}
+
+	if stat != -1 {
+		conditions = append(conditions, "submission.status = ? ")
+		placeholders = append(placeholders, stat)
+	}
+
+
+	where := strings.Join(conditions, "and ")
+
+	if len(where) != 0 {
+		where = "where " + where
+	}
+
+	rows, err := dm.db.DB().Query(query + where, placeholders...)
+	
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	views := make([]SubmissionView, 0, 50)
+
+	for rows.Next() {
+		var sv SubmissionView
+
+		var status int64
+		var prog string
+		rows.Scan(&sv.SubmitTime, &sv.Cid, &sv.Pidx, &sv.Name, &sv.Uid, &sv.UserName, &sv.Lang, &sv.Score, &status, &prog, &sv.Time, &sv.Mem, &sv.Sid)
+
+		if status == 1 {
+			sv.Status = prog
+		}else {
+			sv.Status = SubmissionStatusToString[SubmissionStatus(status)]
+		}
+	}
+
+	return &views, nil
 }
